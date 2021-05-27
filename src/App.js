1c1caf91325e9fe3fbe9ae81fa4fd5e95c3c479d
Sukhrob <senigama1990@mail.ru>
2021-05-27 22:10:29 +0500

    finish

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Modal from './components/Modal';
 
 function App() {
-    const [modalWindow, setModalWindow] = useState(false)
+    const [modalWindow, setModalWindow] = useState(true)
+
+    const closeModalByBtnX = () => {
+        setModalWindow(!modalWindow)
+    }
     return (
         <div className="App">
-            <button onClick={()=> setModalWindow(true)} className='open-modal'>Open modal</button>
-            <Modal modalWindow={modalWindow} setModalWindow={ setModalWindow }/>
+            <button onClick={() => setModalWindow(!modalWindow)} className='open-modal'>Open modal</button>
+            <Modal modalWindow={modalWindow} setModalWindow={setModalWindow} closeModalByBtnX={closeModalByBtnX}/>
         </div>
     );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ModalContent from './ModalContent'
 
-function Modal({ modalWindow, setModalWindow }) {
+function Modal({ modalWindow, closeModalByBtnX }) {
     return (
-        <div className='modal-wrapper'>
+        <div className='modal-wrapper'
+            style={{
+                top: modalWindow ? '-400px': ''
+            }}
+        >
             <div className="modal-header">
                 <p>Modal window</p>
-                <span className='close-modal=btn'>x</span>
+                <span onClick={closeModalByBtnX} className='close-modal=btn'>x</span>
             </div>
             <ModalContent/>
         </div>
